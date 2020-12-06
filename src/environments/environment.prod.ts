1b702a58d95814b33df60dc01ab1954146b12996
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2020-12-06 17:49:00 +0100

    Add environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9iGfpD2hYjEYS2rBtvfoqqneK1E2okAE',
+    authDomain: 'infocorner-2020.firebaseapp.com',
+    databaseURL: 'https://infocorner-2020.firebaseio.com',
+    projectId: 'infocorner-2020',
+    storageBucket: 'infocorner-2020.appspot.com',
+    messagingSenderId: '1056137457400',
+    appId: '1:1056137457400:web:13a6fd59d1a9b0683f950d',
+    measurementId: 'G-SKEY3QJQRL'
+  }
 };
