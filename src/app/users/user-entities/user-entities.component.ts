02325ffe4353b4d87e12cb5016b693bb0e317b46
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2020-12-08 20:13:38 +0100

    Add data as Input in user-entities

--- src/app/users/user-entities/user-entities.component.ts
+++ src/app/users/user-entities/user-entities.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 
 import { IEntity, Entity } from '@models/entity';
@@ -8,9 +9,10 @@ import { IEntity, Entity } from '@models/entity';
   templateUrl: './user-entities.component.html',
   styleUrls: ['./user-entities.component.scss']
 })
-export class UserEntitiesComponent {
+export class UserEntitiesComponent implements OnInit {
 
   @Input() entities: IEntity[];
+  dataSource: MatTableDataSource<IEntity>;
 
   displayedColumns: string[] = [ 'image', 'active', 'id', 'name'];
 
@@ -19,7 +21,16 @@ export class UserEntitiesComponent {
   ) {
   }
 
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.entities);
+  }
+
   public gotoEntity(entity: IEntity): void {
     this.router.navigate([`${Entity.PATH_URL}/${entity.id}`]);
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
