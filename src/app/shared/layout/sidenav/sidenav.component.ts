648b8a34999ec061fe4bdaa4bb8b945406435fc8
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2020-12-08 16:29:23 +0100

    Create list of links in sidebar

--- src/app/shared/layout/sidenav/sidenav.component.ts
+++ src/app/shared/layout/sidenav/sidenav.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  public LINKS = [
+    {
+      icon: 'apps',
+      path: 'admin',
+    },
+    {
+      icon: 'account_circle',
+      path: 'usuarios',
+    },
+    {
+      icon: 'campaign',
+      path: 'avisos',
+    },
+    {
+      icon: 'event',
+      path: 'eventos',
+    },
+    {
+      icon: 'group_work',
+      path: 'entidades',
+    },
+    {
+      icon: 'place',
+      path: 'lugares',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
